0cdf6b3fca38426f3e91f43d7fb0d1b545137c1d
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-10 20:21:20 -0500

    print board

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,17 @@
+state = [[" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
+
+def print_board(state):
+  b = ["--|---|--"] * 5
+  sep = " | "
+  b[0] = sep.join(state[0])
+  b[2] = sep.join(state[1])
+  b[4] = sep.join(state[2])
+  for l in b:
+    print(l)
+
 token = input("Select x or o: ")
 print("You select", token)
+
+print_board(state)
