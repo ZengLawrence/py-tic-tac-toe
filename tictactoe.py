51a6a33f1a2f57275a5a3f3c3f594277ac5d1871
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-16 20:06:05 -0500

    Take turns (#3)

--- tictactoe.py
+++ tictactoe.py
@@ -11,20 +11,24 @@ def print_board(state):
   for l in b:
     print(l)
 
-def enter_move():
+next_moves = { 'x': 'o', 'o': 'x'}
+
+def enter_move(side):
   # get user entered move and return (row, col)
-  help = "Enter 2 digits for the move, for example, 11 for row 1 and column 1."
-  move = input("Enter a move:")
+  help = "Enter 2 digits for the move, for example, 11 for row 1 and column 1. 'q' to quit."
+  move = input("Enter a move for " + side + ": ")
+  if move == 'q':
+    exit()
   if (len(move) == 2):
     try:
       row = int(move[0])
       col = int(move[1])
-      if ((row > 0 and row < 3) and (col > 0 and col < 3)):
-        return (row, col)
+      if ((row > 0 and row < 4) and (col > 0 and col < 4)):
+        return (row, col, side, next_moves[side])
     except ValueError:
       pass
   print(help)
-  return enter_move()
+  return enter_move(side)
 
 def select_token():
   token = input("Select x or o: ")
@@ -37,7 +41,9 @@ def select_token():
 token = select_token()
 print("You select", token)
 print_board(state)
+side = 'x'
 while True:
-  row, col = enter_move()
-  state[row-1][col-1] = token
+  row, col, side, next_side = enter_move(side)
+  state[row-1][col-1] = side
   print_board(state)
+  side = next_side
