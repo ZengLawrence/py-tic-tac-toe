c08c3d3379b7fb7dca36a1f876d36a672010e532
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-24 10:03:03 -0500

    keep state mutation logic together

--- tictactoe.py
+++ tictactoe.py
@@ -29,11 +29,19 @@ def enter_move(side):
   print(instruction)
   return enter_move(side)
 
+def get(state, move):
+  row, col = move
+  return state[row-1][col-1]
+
+def set(state, move, side):
+  row, col = move
+  state[row-1][col-1] = side
+
 def validate(move, state):
   row, col = move
   if not ((row > 0 and row < 4) and (col > 0 and col < 4)):
     return (False, instruction)
-  if not (state[row-1][col-1] == " "):
+  if not (get(state, move) == " "):
     return (False, "Box %s%s is taken" % move)
   return (True, None)
 
@@ -54,8 +62,7 @@ while True:
   move, curr_side, next_side = enter_move(side)
   is_valid, err_msg = validate(move, state)
   if is_valid:
-    row, col = move
-    state[row-1][col-1] = curr_side
+    set(state, move, curr_side)
     print_board(state)
     side = next_side
   else:
