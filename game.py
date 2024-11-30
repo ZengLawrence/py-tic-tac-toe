c10714240e034183a7f5ef654013d3b50251120a
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-11-30 16:59:18 -0500

    Lint codebase (#7)
    Follow PEP8 convention.

--- game.py
+++ game.py
@@ -1,82 +1,97 @@
+""" Module implements game rules"""
+
 def init_state():
-  return [[" ", " ", " "],
-         [" ", " ", " "],
-         [" ", " ", " "]]
+    """ Initialize state"""
+    return [[" ", " ", " "],
+           [" ", " ", " "],
+           [" ", " ", " "]]
 
-def get(state, move):
-  row, col = move
-  return state[row-1][col-1]
+def get_side(state, move):
+    """Utility function to get side taken the box"""
+    row, col = move
+    return state[row-1][col-1]
 
-def set(state, move, side):
-  row, col = move
-  state[row-1][col-1] = side
+def set_side(state, move, side):
+    """Utility function to set side for the box"""
+    row, col = move
+    state[row-1][col-1] = side
 
-class RuleViolation(Exception): pass
-class InvalidMoveViolation(RuleViolation): pass
-class BoxTakenViolation(RuleViolation): pass
+class RuleViolation(Exception):
+    """This is parent Exception class for rule violation."""
+
+class InvalidMoveViolation(RuleViolation):
+    """This exception raised when user makes an invalid move."""
+class BoxTakenViolation(RuleViolation):
+    """This exception raised when user put a piece to a box already taken,"""
 
 def validate(move, state):
-  row, col = move
-  if not ((row > 0 and row < 4) and (col > 0 and col < 4)):
-    raise InvalidMoveViolation()
-  if not (get(state, move) == " "):
-    raise BoxTakenViolation()
+    """Check if move is valid"""
+    row, col = move
+    if not ((0 < row < 4) and (0 < col < 4)):
+        raise InvalidMoveViolation()
+    if not get_side(state, move) == " ":
+        raise BoxTakenViolation()
 
 def next_move(side):
-  if side == 'x':
-    return 'o'
-  else:
+    """Return side for next move"""
+    if side == 'x':
+        return 'o'
     return 'x'
 
 def all_three_same(row):
-  x, y, z = row
-  return (x in 'xo') and (x == y) and (x == z)
+    """Return true if all three in a row is the same side"""
+    x, y, z = row
+    return (x in 'xo') and (x == y) and (x == z)
 
 def winning(state):
-  return (
-    all_three_same(state[0]) or 
-    all_three_same(state[1]) or 
-    all_three_same(state[2]) or 
-    all_three_same([state[0][0], state[1][0], state[2][0]]) or 
-    all_three_same([state[0][1], state[1][1], state[2][1]]) or 
-    all_three_same([state[0][2], state[1][2], state[2][2]]) or 
-    all_three_same([state[0][0], state[1][1], state[2][2]]) or 
-    all_three_same([state[0][2], state[1][1], state[2][0]])
-    )
+    """Return true if there is a winning row, column or diagonal"""
+    return (
+      all_three_same(state[0]) or
+      all_three_same(state[1]) or
+      all_three_same(state[2]) or
+      all_three_same([state[0][0], state[1][0], state[2][0]]) or
+      all_three_same([state[0][1], state[1][1], state[2][1]]) or
+      all_three_same([state[0][2], state[1][2], state[2][2]]) or
+      all_three_same([state[0][0], state[1][1], state[2][2]]) or
+      all_three_same([state[0][2], state[1][1], state[2][0]])
+      )
 
 def stalemate(state):
-  return all(val in 'xo' for row in state for val in row)
+    """Return true if game ends in stalemate"""
+    return all(val in 'xo' for row in state for val in row)
 
 class Game:
-  def __init__(self):
-    self.state = init_state()
-    self.side = 'x'
-    self.result = None
+    """Class representing game"""
 
-  def running(self):
-    return self.result is None
+    def __init__(self):
+        self.state = init_state()
+        self.side = 'x'
+        self.result = None
 
-  def make(self, move):
-    # make a move -- tuple of (row, col) in the game, through exception if not valid
-    validate(move, self.state)
-    set(self.state, move, self.side)
-    if winning(self.state):
-      self.result = self.side + " won" 
-    if stalemate(self.state):
-      self.result = "stalemate"
-    self.side = next_move(self.side)
+    def running(self):
+        """Return true if game is running. Game ends if there is a winner or a stalemate."""
+        return self.result is None
 
+    def make(self, move):
+        """Make a move -- tuple of (row, col) in the game, through exception if not valid"""
+        validate(move, self.state)
+        set_side(self.state, move, self.side)
+        if winning(self.state):
+            self.result = self.side + " won"
+        if stalemate(self.state):
+            self.result = "stalemate"
+        self.side = next_move(self.side)
 
 if __name__ == "__main__":
-  assert winning([['x', 'x', 'x'], [' ', ' ', ' '], [' ', ' ', ' ']]), "first row is winner"
-  assert winning([[' ', ' ', ' '], ['x', 'x', 'x'], [' ', ' ', ' ']]), "second row is winner"
-  assert winning([[' ', ' ', ' '], [' ', ' ', ' '], ['x', 'x', 'x']]), "third row is winner"
-  assert winning([['o', ' ', ' '], ['o', ' ', ' '], ['o', ' ', ' ']]), "first column is winner"
-  assert winning([[' ', 'o', ' '], [' ', 'o', ' '], [' ', 'o', ' ']]), "second column is winner"
-  assert winning([[' ', ' ', 'o'], [' ', ' ', 'o'], [' ', ' ', 'o']]), "third column is winner"
-  assert winning([['x', ' ', ' '], [' ', 'x', ' '], [' ', ' ', 'x']]), "diagonal is winner"
-  assert winning([[' ', ' ', 'o'], [' ', 'o', ' '], ['o', ' ', ' ']]), "diagonal is winner"
-  assert stalemate([['o', 'x', 'x'], ['x', 'x', 'o'], ['o', 'o', 'x']]), "stalemate"
+    assert winning([['x', 'x', 'x'], [' ', ' ', ' '], [' ', ' ', ' ']]), "first row is winner"
+    assert winning([[' ', ' ', ' '], ['x', 'x', 'x'], [' ', ' ', ' ']]), "second row is winner"
+    assert winning([[' ', ' ', ' '], [' ', ' ', ' '], ['x', 'x', 'x']]), "third row is winner"
+    assert winning([['o', ' ', ' '], ['o', ' ', ' '], ['o', ' ', ' ']]), "first column is winner"
+    assert winning([[' ', 'o', ' '], [' ', 'o', ' '], [' ', 'o', ' ']]), "second column is winner"
+    assert winning([[' ', ' ', 'o'], [' ', ' ', 'o'], [' ', ' ', 'o']]), "third column is winner"
+    assert winning([['x', ' ', ' '], [' ', 'x', ' '], [' ', ' ', 'x']]), "diagonal is winner"
+    assert winning([[' ', ' ', 'o'], [' ', 'o', ' '], ['o', ' ', ' ']]), "diagonal is winner"
+    assert stalemate([['o', 'x', 'x'], ['x', 'x', 'o'], ['o', 'o', 'x']]), "stalemate"
 
-  running_game = [['o', 'x', 'x'], [' ', 'x', 'o'], ['o', 'o', 'x']]
-  assert not (winning(running_game) or stalemate(running_game)), "game is still running"
+    running_game = [['o', 'x', 'x'], [' ', 'x', 'o'], ['o', 'o', 'x']]
+    assert not (winning(running_game) or stalemate(running_game)), "game is still running"
--- tictactoe.py
+++ tictactoe.py
@@ -1,42 +1,46 @@
-from game import Game, BoxTakenViolation
+"""Main module to start the console based game."""
+
+import sys
+from game import Game, BoxTakenViolation, RuleViolation
 
 def print_board(state):
-  b = ["--|---|--"] * 5
-  sep = " | "
-  b[0] = sep.join(state[0])
-  b[2] = sep.join(state[1])
-  b[4] = sep.join(state[2])
-  for l in b:
-    print(l)
+    """Print game to the console."""
+    b = ["--|---|--"] * 5
+    sep = " | "
+    b[0] = sep.join(state[0])
+    b[2] = sep.join(state[1])
+    b[4] = sep.join(state[2])
+    for l in b:
+        print(l)
 
-instruction = "Enter 2 digits for the move, for example, 11 for row 1 and column 1. 'q' to quit."
+INSTRUCTION = "Enter 2 digits for the move, for example, 11 for row 1 and column 1. 'q' to quit."
 
 def enter_move(side):
-  # get user entered move and return (row, col)
-  move = input("Enter a move for " + side + ": ")
-  if move == 'q':
-    exit()
-  if (len(move) == 2):
-    try:
-      row = int(move[0])
-      col = int(move[1])
-      return (row, col)
-    except ValueError:
-      pass
-  print(instruction)
-  return enter_move(side)
+    """get user entered move and return (row, col)"""
+    move_input = input("Enter a move for " + side + ": ")
+    if move_input == 'q':
+        sys.exit()
+    if len(move_input) == 2:
+        try:
+            row = int(move_input[0])
+            col = int(move_input[1])
+            return (row, col)
+        except ValueError:
+            pass
+    print(INSTRUCTION)
+    return enter_move(side)
 
-print(instruction)
+print(INSTRUCTION)
 game = Game()
 print_board(game.state)
 while game.running():
-  move = enter_move(game.side)
-  try:
-    game.make(move)
-  except BoxTakenViolation:
-    print("Box %s%s is taken" % move)
-  except:
-    print(instruction)
-  else:
-    print_board(game.state)
+    move = enter_move(game.side)
+    try:
+        game.make(move)
+    except BoxTakenViolation:
+        print(f"Box {move[0]}{move[1]} is taken")
+    except RuleViolation:
+        print(INSTRUCTION)
+    else:
+        print_board(game.state)
 print(game.result)
