a34acdc7192986549222018dcb3fce6d088ade4b
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-15 10:44:01 -0500

    properly initialize board

--- gui.py
+++ gui.py
@@ -2,9 +2,16 @@
 
 from tkinter import Tk
 from tkinter import ttk
+
+def init_button(frame, col, row):
+    """Initialize button in board"""
+    btn = ttk.Button(frame, text=" ")
+    btn.grid(column=col, row=row)
+    return btn
+
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-board = [ttk.Button(frm, text=" ").grid(column=i, row=j) for i in range(3) for j in range(3)]
+board = [init_button(frm, col=i, row=j) for i in range(3) for j in range(3)]
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=4)
 root.mainloop()
