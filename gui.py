04eea6e475ad9e721121080cc5ac1b0b84950229
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-15 11:42:50 -0500

    initialize from game state

--- gui.py
+++ gui.py
@@ -2,18 +2,25 @@
 
 from tkinter import Tk
 from tkinter import ttk
+from game import Game
 
-def init_button(frame, col, row):
+def init_button(frame, text, col, row):
     """Initialize button in board"""
     def update_text():
         btn["text"] = "clicked!"
-    btn = ttk.Button(frame, text=" ", command=update_text)
+    btn = ttk.Button(frame, text=text, command=update_text)
     btn.grid(column=col, row=row)
     return btn
+
+def init_board(frame, game):
+    """Initialize board with game state"""
+    return [init_button(frame, cell, col=i, row=j)
+            for (j, row) in enumerate(game.state)
+            for (i, cell) in enumerate(row)]
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-board = [init_button(frm, col=i, row=j) for i in range(3) for j in range(3)]
+board = init_board(frm, Game())
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=4)
 root.mainloop()
