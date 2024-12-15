f6414f4028e7954eebbbac29e26760cbd9076fa8
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-15 12:34:12 -0500

    register callback for state change event

--- gui.py
+++ gui.py
@@ -14,13 +14,21 @@ def init_button(frame, text, game, col, row):
 
 def init_board(frame, game):
     """Initialize board with game state"""
-    return [init_button(frame, btn_text, game, col=i, row=j)
-            for (j, row) in enumerate(game.state)
-            for (i, btn_text) in enumerate(row)]
+    return [[init_button(frame, btn_text, game, col=i, row=j)
+            for (i, btn_text) in enumerate(row)]           
+            for (j, row) in enumerate(game.state)]
+
+def refresh_board(board, state):
+    for (i, row) in enumerate(state):
+        for (j, btn_text) in enumerate(row):
+            btn = board[i][j]
+            btn["text"] = btn_text
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-board = init_board(frm, Game())
+game = Game()
+board = init_board(frm, game)
+game.register(lambda game: refresh_board(board, game.state))
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=4)
 root.mainloop()
