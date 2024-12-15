1aeef2abee702966166ba3dfcb31343c66db1fb7
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-15 10:23:59 -0500

    initial board

---
+++ gui.py
@@ -0,0 +1,8 @@
+from tkinter import Tk
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+board = [ttk.Button(frm, text=" ").grid(column=i, row=j) for i in range(3) for j in range(3)]
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=4)
+root.mainloop()
