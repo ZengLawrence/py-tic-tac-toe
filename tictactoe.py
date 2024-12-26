54184eaaf3afe92d5ef9a80502c36a00fea449b9
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-26 11:15:53 -0500

    generated by co-pilot

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,21 @@
-"""Main module to start the console based game."""
+"""Main module to start the game."""
 
+import argparse
+import tkinter as tk
 from console import Console
+from gui import App
 
-Console().start()
+def main():
+    parser = argparse.ArgumentParser(description="Start the Tic Tac Toe game.")
+    parser.add_argument('--gui', action='store_true', help="Run the game with a GUI")
+    args = parser.parse_args()
+
+    if args.gui:
+        root = tk.Tk()
+        app = App(root)
+        root.mainloop()
+    else:
+        Console().start()
+
+if __name__ == "__main__":
+    main()
