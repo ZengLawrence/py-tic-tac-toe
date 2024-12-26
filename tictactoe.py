c564d12b6de9b3ab82e338849c851ffbc3350d9d
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-26 11:34:40 -0500

    Add command line flag to start GUI (#20)

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,29 @@
-"""Main module to start the console based game."""
+"""Main module to start the game."""
 
+import argparse
 from console import Console
 
-Console().start()
+try:
+    from gui import App
+    GUI_AVAILABLE = True
+except ImportError:
+    GUI_AVAILABLE = False
+
+
+def main():
+    """Main function to start the game."""
+    desc = "Tic Tac Toe game. By default, runs in console mode."
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('--gui', action='store_true', help="Start the game with GUI")
+    args = parser.parse_args()
+
+    if args.gui:
+        if not GUI_AVAILABLE:
+            print("GUI is not available. Please ensure tkinter is installed.")
+            return
+        App().run()
+    else:
+        Console().start()
+
+if __name__ == "__main__":
+    main()
