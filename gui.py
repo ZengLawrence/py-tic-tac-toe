ce5a5822367b852b6c98297a45579a1b09a6b9c7
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-26 09:45:19 -0500

    Show game status (#17)

--- gui.py
+++ gui.py
@@ -25,24 +25,43 @@ def refresh_board(board, state):
             btn = board[i][j]
             btn["text"] = btn_text
 
+def init_status(frame, game):
+    """Initialize status label"""
+    return ttk.Label(frame, text=status_text(game))
+
+def refresh_status(status, game):
+    """Refresh status with game result"""
+    status["text"] = status_text(game)
+
+def status_text(game):
+    """Return status text"""
+    return game.result if game.result else game.side + "'s turn. Click on a box"
+
 class App:
     """Class for GUI app"""
     def __init__(self, master):
         self.root = master
-        self.root.title("Tic Tac Too")
+        self.root.title("Tic Tac Toe")
         self.frm = ttk.Frame(self.root, padding=10)
         self.frm.grid()
-        ttk.Label(self.frm, text="Click on a box").grid()
+        game = Game()
+        self.status = init_status(self.frm, game)
+        self.status.grid(sticky="w")
         self.board_frame = ttk.Frame(self.frm)
         self.board_frame.grid()
-        self.game = Game()
-        self.board = init_board(self.board_frame, self.game)
-        self.game.register(lambda game: refresh_board(self.board, game.state))
+        self.board = init_board(self.board_frame, game)
+        game.register(self.__refresh)
         ttk.Button(self.frm, text="Quit", command=self.root.destroy).grid()
 
     def run(self):
         """Run the app"""
         self.root.mainloop()
+    
+    def __refresh(self, game):
+        """Refresh the app"""
+        refresh_status(self.status, game)
+        refresh_board(self.board, game.state)
+        self.root.update()
 
 if __name__ == "__main__":
     root = Tk()
